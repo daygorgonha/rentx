bfe5a7d76d2667e7a59a897985d72b250376452c
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-14 18:06:35 -0300

    add feature

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,13 @@
+import { ICreateCategoryDTO } from "./ICategoriesRepository";
+
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ description, name }: ICreateSpecificationDTO): void;
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationDTO };
