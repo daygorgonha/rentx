529b481a3841cbcf4b8e92d747b02a0702d7faee
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-15 10:50:26 -0300

    add feature

--- src/modules/cars/useCases/createSpecification/index.ts
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -1,6 +1,6 @@
 import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
 import { CreateSpecificationController } from "./CreateSpecificationController";
-import { CreateSpecificationUsecase } from "./CreateSpecificationUseCase";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 const specificationsRepository = new SpecificationsRepository();
 
