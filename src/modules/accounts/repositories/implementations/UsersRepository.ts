1c877a5aef7ff69dcf873848d6bdc0196736b701
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-21 15:46:31 -0300

    criptografia de senha

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -11,7 +11,12 @@ class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async create({name, email, driver_license, password}: ICreateUserDTO): Promise<void> {
+  async create({
+    name, 
+    email, 
+    driver_license, 
+    password,
+  }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,  
       email, 
@@ -20,6 +25,11 @@ class UsersRepository implements IUsersRepository {
     });
 
     await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
   }
 }
 
