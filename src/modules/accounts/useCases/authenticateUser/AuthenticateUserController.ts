718b65dd512688c779d59f709421c496e03e23dd
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-21 17:29:53 -0300

    criação de token de usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+
+class AutheticateUserController {
+  async handle(resquest: Request, response: Response): Promise<Response>{
+    const { password, email } = resquest.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    const token = await authenticateUserUseCase.execute({
+      password,
+      email,
+    });
+
+    return response.json(token);
+  }
+
+}
+
+export { AutheticateUserController }
---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { AutheticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AutheticateUserController();
+
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
+
+export { authenticateRoutes };
