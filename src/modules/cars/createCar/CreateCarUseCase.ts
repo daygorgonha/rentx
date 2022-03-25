60ad25b1606043340ed162a1674c0bbe809822d3
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-25 14:18:57 -0300

    Estruturando a entidade de carros

--- src/modules/cars/createCar/CreateCarUseCase.ts
+++ src/modules/cars/createCar/CreateCarUseCase.ts
@@ -1,4 +1,6 @@
+import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
+import { Car } from "../infra/typeorm/entities/Car";
 import { ICarsRepository } from "../repositories/ICarsRepository";
 
 interface IRequest {
@@ -11,10 +13,10 @@ interface IRequest {
   category_id: string;
 }
 
-@injectable()
+//@injectable()
 class CreateCarUseCase {
   constructor(
-    @inject("CarsRepository")
+    //@inject("CarsRepository")
     private carsRepository: ICarsRepository
   ) {}
 
@@ -26,8 +28,17 @@ class CreateCarUseCase {
     fine_amount,
     brand,
     category_id,
-  }: IRequest): Promise<void> {
-    this.carsRepository.create({
+  }: IRequest): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError("Car already exists!")
+    }
+
+
+    const car = await this.carsRepository.create({
       name,
       description,
       daily_rate,
@@ -36,6 +47,8 @@ class CreateCarUseCase {
       brand,
       category_id,
     });
+
+    return car;
   }
 }
 
