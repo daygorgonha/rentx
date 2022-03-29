4c5345c4824503de25078f23146072c8fd1b8a8e
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-29 18:06:28 -0300

    Caso de uso do cadastro de imagens do carro

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -4,12 +4,18 @@ import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
+import multer from "multer";
+import uploadConfig from "@config/upload";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 
 carsRoutes.post(
@@ -27,5 +33,15 @@ carsRoutes.post(
   ensureAdmin,
   createCarSpecificationController.handle
 );
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array("images"),
+  uploadCarImagesController.handle
+);
+
+
 
 export { carsRoutes };
