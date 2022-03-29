1e4d9acf5856e9e9a97633d39cceb5b5488d9b00
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-03-29 16:13:59 -0300

    Finalizando CreateCarSpecificationUseCase

--- src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -4,7 +4,7 @@ import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecific
 class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
   specifications: Specification[] = [];
 
-  async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+  async create({ description, name }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = new Specification();
 
     Object.assign(specification, {
@@ -13,6 +13,8 @@ class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
     });
 
     this.specifications.push(specification);
+
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
