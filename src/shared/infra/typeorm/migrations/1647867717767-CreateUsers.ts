bb98ecb022ae9282da132644dfdae0b81e84c525
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-04-06 19:20:03 -0300

    Criando os testes do aluguel

--- src/shared/infra/typeorm/migrations/1647867717767-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1647867717767-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1647867717767 implements MigrationInterface {
               {
                 name: "id",
                 type: "uuid"
+                isPrimary: true,
               },
               {
                 name: "name",
