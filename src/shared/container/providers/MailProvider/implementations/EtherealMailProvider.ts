351da26d6c571bdfbff0b75a6a339d4f0d50eb7e
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-04-22 12:04:30 -0300

    Inserindo template engine para envio de email

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,12 +1,14 @@
 import { injectable } from "tsyringe";
 import { IMailProvider } from "../IMailProvider";
-import nodemailer from "nodemailer";
+import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
   constructor() {
     nodemailer.createTestAccount().then((account) => {
-      const transorter = nodemailer.createTransport({
+      const transporter = nodemailer.createTransport({
         host: account.smtp.host,
         port: account.smtp.port,
         secure: account.smtp.secure,
@@ -18,16 +20,26 @@ class EtherealMailProvider implements IMailProvider {
 
       this.client = transporter;
     })
-    .catch((err) => console.error(err);)
+    .catch((err) => console.error(err));
   }
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
-    const message = await this.client.sendMail({
-      to,
-      from: "Rentx <noqreplay@rentx.com.br>",
-      subject,
-      text: body,
-      html: body,
-    });
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+    ): Promise<void> {
+      const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+      const templateParse = handlebars.compile(templateFileContent);
+
+      const templateHTML = templateParse(variables);
+
+     const message = await this.client.sendMail({
+        to,
+        from: "Rentx <noqreplay@rentx.com.br>",
+        subject,
+        html: templateHTML,
+      });
 
     console.log("Message sent: %s", message.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
